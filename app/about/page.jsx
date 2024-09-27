67211a5f91e533a123432874446dfbe5fe14d52b
Khushbu Yadav <khushbuyad123@gmail.com>
2024-09-27 07:43:08 +0530

    new

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+      <p>This is me , khushbu yadav </p>
+    </div>
+  )
+}
+
+export default Page
--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <>
     <p>Let's Build Progressive web app</p>
+    <Link href="/about">About</Link>
     </>
   );
 }
