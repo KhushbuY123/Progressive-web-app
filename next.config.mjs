622f2b8280b59dd265465bc45de9d5a00db656ea
Khushbu Yadav <khushbuyad123@gmail.com>
2024-09-27 07:41:13 +0530

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
+const withPWA = withPWAInit({
+    cacheOnFrontEndNav:true,
+    aggressiveFrontEndNavCaching:true,
+    reloadOnOnline:true,
+    swcMinify:true,
+    disable: false,
+    workboxOptions:{
+    disableDevLogs:true,
+  }
+});
+const nextConfig = withPWA({});
 
 export default nextConfig;
